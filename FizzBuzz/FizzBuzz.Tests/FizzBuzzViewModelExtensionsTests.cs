c0ba00672b121e7d11fed7f1340e9d12e76487fc
Sai <andol.sairam@gmail.com>
2016-07-12 09:11:35 +0100

    Step3- complete

---
+++ FizzBuzz/FizzBuzz.Tests/FizzBuzzViewModelExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using FizzBuzz.Models;
+using NUnit.Framework;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+    public class FizzBuzzViewModelExtensionsTests
+    {
+        [Test]
+        public void GetDisplayStyle_ReturnsBlueForFizz()
+        {
+            var fizzBuzzViewModel = new FizzBuzzViewModel() {Number = 3, Display = "fizz"};
+
+            var displayStyle = fizzBuzzViewModel.GetDisplayStyle();
+
+            Assert.That(displayStyle == "background-color:blue");
+        }
+
+        [Test]
+        public void GetDisplayStyle_ReturnsGreenForBuzz()
+        {
+            var fizzBuzzViewModel = new FizzBuzzViewModel() {Number = 5, Display = "buzz"};
+
+            var displayStyle = fizzBuzzViewModel.GetDisplayStyle();
+
+            Assert.That(displayStyle == "background-color:green");
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Models/FizzBuzzViewModelExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace FizzBuzz.Models
+{
+    public static class FizzBuzzViewModelExtensions
+    {
+        public static string GetDisplayStyle(this FizzBuzzViewModel fizzBuzzViewModel)
+        {
+            switch (fizzBuzzViewModel.Display.Trim().ToLower())
+            {
+                case "fizz":
+                    return "background-color:blue";
+                case "buzz":
+                    return "background-color:green";
+            }
+            return "background-color:grey";
+        }
+    }
+}
